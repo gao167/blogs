a6f14943e42ee24c3385986c34a65605c292adc8
Hung <dsfdsfdsf>
2019-03-26 00:47:44 +0700

    fake data

--- blog-api/routes/index.js
+++ blog-api/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const data = [
+  {id: 1, name: 'Blog 1'},
+  {id: 2, name: 'Blog 2'},
+  {id: 3, name: 'Blog 3'},
+  {id: 4, name: 'Blog 4'},
+  {id: 5, name: 'Blog 5'},
+  {id: 6, name: 'Blog 6'},
+  {id: 7, name: 'Blog 7'},
+  {id: 8, name: 'Blog 8'},
+]
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({
+    data
+  })
 });
 
 module.exports = router;
