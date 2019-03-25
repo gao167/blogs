d14746e798b295df40ee5abf284278994199d07a
Hung <dsfdsfdsf>
2019-03-26 01:58:35 +0700

    them module cors ket noi api va frontend, thay doi state ,get data va loading

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react'
 import './App.css'
 import axios from 'axios'
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      loading: false,
+      blogs: []
+    }
+  }
   componentWillMount = () => {
+    this.setState({
+      loading: true
+    })
     axios.get('http://localhost:4000/')
-      .then(res=> console.log(res))
+      .then(res=> {
+        this.setState({
+          blogs: res.data.data,
+          loading: false
+        })
+      })
       .catch(err => console.log(err))
   }
   
   render() {
+    const {blogs, loading} = this.state
+    if(loading){
+      return <p>Loading!!!</p>
+    }
     return (
       <div className="App">
-        1
+        <ul>
+          {blogs.map((blog)=><li key={blog.id}>{blog.name}</li>)}
+        </ul>
       </div>
     );
   }
